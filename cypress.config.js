350c9517d11ae43164cb3fd8ac2aa18ee725e7d8
Max Urbanija <urbanijamax@gmail.com>
2024-02-29 10:55:20 -0300

    cypress cloud configs added

--- cypress.config.js
+++ cypress.config.js
@@ -1,6 +1,7 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  projectId: "zgnv6i",
   video: true,
   screenshotOnRunFailure: true,
   reporter: 'cypress-mochawesome-reporter',
