f26b53c761e547e1b876e6d7a4363779ff65f752
maxurbanija <urbanijamax@gmail.com>
2025-03-25 10:28:39 -0300

    file renamed

---
+++ cypress/e2e/exchangeRates.js
@@ -0,0 +1,22 @@
+import{
+    searchInput,
+    wikipediaSearchResult
+} from '../locators/googlePage';
+
+describe('Verify DolarHoy values', () => {
+it( "Validate DolarHoy displays exchange rates", () => {
+    cy.visit( 'https://dolarhoy.com/' );
+    cy.url().should("include","dolarhoy");
+    
+    cy.get('.compra .val')
+        .should('be.visible');
+    
+    cy.screenshot('ExchangeRates');
+
+
+  })
+
+  it("Check web",( ) => {
+    cy.visit('https://sweetshop.netlify.app/sweets');
+  })
+})
